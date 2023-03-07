d27f826bf814d704f3c69506a9aa54e61b5d3353
Andrey Fedorov <fvandre@outlook.com>
2023-03-07 18:20:53 +0300

    Final code

--- src/test/java/ru/netology/services/servicesTest.java
+++ src/test/java/ru/netology/services/servicesTest.java
@@ -1,46 +1,21 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-//import org.junit.jupiter.params.provider.CsvSource;
-//import ru.netology.services.services;
+
 
 public class servicesTest {
 
     @ParameterizedTest
-    //@CsvSource({
-    //"10000,3000,20000,3",
-    //"100000,60000,150000,2"
-    //})
     @CsvFileSource(files = "src/test/resources/monthRest.csv")
-    public void calculateMonthRestA(int income, int expense, int threshold, int expected) {
+
+    public void calculateMonthRest(int income, int expense, int threshold, int expected) {
 
         services service = new services();
 
-        //int income = 10_000;
-        //int expense = 3_000;
-        //int threshold = 20_000;
-
-        //int expected = 3;
         int actual = service.calcMonthsRest(income, expense, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-
-    //@Test
-    //public void calculateMonthRestB() {
-
-    //services service = new services();
-
-    //int income = 100_000;
-    //int expense = 60_000;
-    //int threshold = 150_000;
-
-    //int expected = 2;
-    //int actual = service.calcMonthsRest(income, expense, threshold);
-
-    //Assertions.assertEquals(expected, actual);
-    //}
 }
